0a00fad846882d4f21deeb3e147dabe28d501048
2678749801 <2678749801@qq.com>
2020-11-26 12:57:16 +0800

    master

--- 笔记.js
+++ 笔记.js
@@ -10,3 +10,9 @@ const [name1, name2, name3] = names
 //timeout 超时的意思是请求过去规定时间没有收到就报错
 //post传递是是数据 data
 //get是参数 param
+/*
+1.app.message修改数据 vue内部如何监听message发生改变
+object.defineProperty 来监听对象属性的改变
+2.当数据发生改变时 vue是如何知道要通知哪些人 界面发生刷新
+发布订阅者模式
+*/
